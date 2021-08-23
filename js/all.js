119ae7abd70377c737dc7ae10771e3745a1b88c6
Chek <zzzxxxccc11225@yahoo.com.tw>
2021-08-23 13:45:45 +0800

    更新

---
+++ js/all.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    // Lightbox 燈箱效果
+    lightbox.option({
+    resizeDuration: 100,
+    wrapAround: true,
+    });
+    
+    // TOP按鈕
+    $('.top-btn a').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0
+    },350);
+    });
+
+    // 漢堡選單
+    $('.hamburger-menu a').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('fa-bars').toggleClass('fa-times').parent().siblings('.menu').toggleClass('menu-open')
+    });
+
+    // 愛心按鈕
+    $('.fa-heart').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('fas').toggleClass('far')
+    });
+
+    // 加入購物車
+    $('.pure-button-cart').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('select')
+    });
+
+});
